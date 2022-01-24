85738a16cc1a3d194300007fe58838964e2bea11
Failesmen <failesmenfalw@gmail.com>
2022-01-24 13:49:08 -0300

    bootstrap has been fixed

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
     return render(request, "pages/index.html")
+
+def contato(request):
+    return render(request, "pages/contato.html")
 
 
